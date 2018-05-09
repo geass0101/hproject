30d9ab1692f010ff8de966d5cfd1bb09eca15243
Unknown <aaronvdp@hotmail.com>
2018-05-09 13:54:06 +0200

    Added all location elements

---
+++ app/Api/Controllers/LocationsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Api\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class LocationsController extends BaseController
+{
+    public function storeLocation(Request $request)
+    {
+        $id=JWTAuth::parseToken()->authenticate()->id;
+        $loc=Localization::find($id);
+        if(isset($loc)){
+            $loc->lat=$request->lat;
+            $loc->long=$request->long;
+            $loc->save();
+        } else {
+            $loc=new Localization;
+            $loc->id=$id;
+            $loc->lat = $request->lat;
+            $loc->long = $request->long;
+        }
+    }
+
+}
---
+++ app/Location.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    protected $fillable = ['id','lat','long'];
+
+   public function user()
+    {
+        return $this->hasOne('App\User');
+    }
+}
